d0decb442c22479c36ba7f5183cfed2277620fd4
Nelson Lopes Silva <5671236+nflsilva@users.noreply.github.com>
2022-05-05 18:11:32 +0100

    feat: added missing file

--- src/android/com/outsystems/firebase/cloudmessaging/OSFirebaseCloudMessaging.kt
+++ src/android/com/outsystems/firebase/cloudmessaging/OSFirebaseCloudMessaging.kt
@@ -1,30 +1,23 @@
 package com.outsystems.firebase.cloudmessaging;
 
-import android.content.Intent
-import com.google.gson.Gson
-import com.outsystems.plugins.sociallogins.apple.AppleHelper
-import com.outsystems.plugins.sociallogins.apple.SocialLoginsAppleController
-import com.outsystems.plugins.sociallogins.facebook.FacebookHelper
-import com.outsystems.plugins.sociallogins.facebook.SocialLoginsFacebookController
-import com.outsystems.plugins.sociallogins.google.GoogleHelper
-import com.outsystems.plugins.sociallogins.google.SocialLoginsGoogleController
-import com.outsystems.plugins.sociallogins.linkedin.SocialLoginsLinkedinController
+import com.outsystems.plugins.sociallogins.CordovaImplementation
 import org.apache.cordova.CallbackContext
-import org.apache.cordova.CordovaInterface
-import org.apache.cordova.CordovaWebView
 import org.json.JSONArray
-import org.json.JSONException
 
-class OSFirebaseCloudMessaging : CordovaImplementation() {
+class OSFirebaseCloudMessaging(override var callbackContext: CallbackContext?) : CordovaImplementation() {
 
     override fun execute(action: String, args: JSONArray, callbackContext: CallbackContext): Boolean {
         when (action) {
-            "loginApple" -> {
+            "getToken" -> {
                 //doLoginApple(args)
             }
             else -> return false
         }
         return true
+    }
+
+    override fun areGooglePlayServicesAvailable(): Boolean {
+        TODO("Not yet implemented")
     }
 
 }
