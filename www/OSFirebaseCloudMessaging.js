5a01728620dc6df4580a8e64cc8b27ec03a58b8b
Carlos Corrêa <carlscorrea@gmail.com>
2022-05-12 16:31:54 +0100

    added new methods to js

--- www/OSFirebaseCloudMessaging.js
+++ www/OSFirebaseCloudMessaging.js
@@ -11,3 +11,19 @@ exports.subscribe = function (topic, success, error) {
 exports.unsubscribe = function (topic, success, error) {
     exec(success, error, 'OSFirebaseCloudMessaging', 'unsubscribe', [topic]);
 };
+
+exports.requestPermission = function ( success, error) {
+    exec(success, error, 'OSFirebaseCloudMessaging', 'requestPermission');
+};
+
+exports.clearNotifications = function (success, error) {
+    exec(success, error, 'OSFirebaseCloudMessaging', 'clearNotifications');
+};
+
+exports.setBadge = function (badge, success, error) {
+    exec(success, error, 'OSFirebaseCloudMessaging', 'setBadge', [badge]);
+};
+
+exports.getBadge = function (success, error) {
+    exec(success, error, 'OSFirebaseCloudMessaging', 'getBadge');
+};
