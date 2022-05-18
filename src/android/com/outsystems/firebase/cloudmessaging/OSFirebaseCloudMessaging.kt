2e996b1b762cb28244cbd75fca158d7d89bc0d78
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-05-18 18:55:04 +0100

    feat: first implementation of the following methods
    getBadgeNumber, setBadgeNumber and ClearNotifications
    
    References:
    https://outsystemsrd.atlassian.net/browse/RMET-1576
    https://outsystemsrd.atlassian.net/browse/RMET-1561
    https://outsystemsrd.atlassian.net/browse/RMET-1557

--- src/android/com/outsystems/firebase/cloudmessaging/OSFirebaseCloudMessaging.kt
+++ src/android/com/outsystems/firebase/cloudmessaging/OSFirebaseCloudMessaging.kt
@@ -1,16 +1,23 @@
 package com.outsystems.firebase.cloudmessaging;
 
+
+import android.content.Context
 import com.outsystems.plugins.firebasemessaging.controller.FirebaseMessagingController
 import com.outsystems.plugins.firebasemessaging.controller.FirebaseMessagingInterface
 import com.outsystems.plugins.firebasemessaging.controller.FirebaseMessagingManager
+import com.outsystems.plugins.firebasemessaging.controller.FirebaseNotificationManager
 import com.outsystems.plugins.firebasemessaging.model.FirebaseMessagingErrors
 import com.outsystems.plugins.oscordova.CordovaImplementation
 import org.apache.cordova.CallbackContext
 import org.json.JSONArray
 
+
 class OSFirebaseCloudMessaging : CordovaImplementation() {
 
     override var callbackContext: CallbackContext? = null
+
+    private val CHANNEL_ID = "com.outsystems.firebasecloudmessaging"
+    private val KEY = "badge"
 
     private val controllerDelegate = object: FirebaseMessagingInterface {
         override fun callbackToken(token: String) {
@@ -24,21 +31,25 @@ class OSFirebaseCloudMessaging : CordovaImplementation() {
         }
     }
     private val messaging = FirebaseMessagingManager()
-    private val controller = FirebaseMessagingController(controllerDelegate, messaging)
+    private val fbNotificationManager = FirebaseNotificationManager()
+    private val controller = FirebaseMessagingController(controllerDelegate, messaging, fbNotificationManager)
 
     override fun execute(action: String, args: JSONArray, callbackContext: CallbackContext): Boolean {
         this.callbackContext = callbackContext
         when (action) {
             "getToken" -> {
-                controller.getToken()
+                //controller.getToken()
+                setBadgeNumber(args)
             }
             "subscribe" -> {
                 val topic = ""
-                controller.subscribe(topic)
+                //controller.subscribe(topic)
+                getBadgeNumber()
             }
             "unsubscribe" -> {
                 val topic = ""
-                controller.unsubscribe(topic)
+                //controller.unsubscribe(topic)
+                clearNotifications()
             }
         }
         return true
@@ -53,5 +64,29 @@ class OSFirebaseCloudMessaging : CordovaImplementation() {
     override fun areGooglePlayServicesAvailable(): Boolean {
         TODO("Not yet implemented")
     }
+
+    private fun getBadgeNumber() {
+        val badgeNumber = controller.getBadgeNumber(cordova.activity)
+        if(badgeNumber != null){
+            sendPluginResult(badgeNumber, null)
+        }
+        else{
+            sendPluginResult(null, null)
+        }
+    }
+
+    private fun setBadgeNumber(args : JSONArray) {
+        val badge = 3
+        controller.setBadgeNumber(cordova.activity, badge)
+    }
+
+    private fun clearNotifications() {
+        controller.clearNotifications(cordova.activity)
+    }
+
+    private fun getResourceId(context: Context, typeAndName: String): Int {
+        return context.resources.getIdentifier(typeAndName, null, context.packageName)
+    }
+
 
 }
