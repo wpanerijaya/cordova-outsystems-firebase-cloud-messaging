171a4931fb3c397c63152afddcaecaeece50f159
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-05-31 17:45:02 +0100

    feat: add JS layer methods

--- www/OSFirebaseCloudMessaging.js
+++ www/OSFirebaseCloudMessaging.js
@@ -35,3 +35,84 @@ exports.registerDevice = function (success, error) {
 exports.unregisterDevice = function (success, error) {
     exec(success, error, 'OSFirebaseCloudMessaging', 'unregisterDevice');
 };
+
+// Event listener
+exports._listener = {};
+
+/**
+ * Register callback for given event.
+ *
+ * @param [ String ]   event    The name of the event.
+ * @param [ Function ] callback The function to be exec as callback.
+ *
+ * @return [ Void ]
+ */
+ exports.on = function (event, callback) {
+    var type = typeof callback;
+
+    if (type !== 'function' && type !== 'string')
+        return;
+
+    if (!this._listener[event]) {
+        this._listener[event] = [];
+    }
+
+    var item = [callback, window];
+
+    this._listener[event].push(item);
+};
+
+/**
+ * Unregister callback for given event.
+ *
+ * @param [ String ]   event    The name of the event.
+ * @param [ Function ] callback The function to be exec as callback.
+ *
+ * @return [ Void ]
+ */
+ exports.un = function (event, callback) {
+    var listener = this._listener[event];
+
+    if (!listener)
+        return;
+
+    for (var i = 0; i < listener.length; i++) {
+        var fn = listener[i][0];
+
+        if (fn == callback) {
+            listener.splice(i, 1);
+            break;
+        }
+    }
+};
+
+/**
+ * Fire the event with given arguments.
+ *
+ * @param [ String ] event The event's name.
+ * @param [ *Array]  args  The callback's arguments.
+ *
+ * @return [ Void]
+ */
+ exports.fireEvent = function (event) {
+    var args     = Array.apply(null, arguments).slice(1),
+        listener = this._listener[event];
+
+    if (!listener)
+        return;
+
+    if (args[0] && typeof args[0].data === 'string') {
+        args[0].data = JSON.parse(args[0].data);
+    }
+
+    for (var i = 0; i < listener.length; i++) {
+        var fn    = listener[i][0],
+            scope = listener[i][1];
+
+        if (typeof fn !== 'function') {
+            fn = scope[fn];
+        }
+
+        fn.apply(scope, args);
+    }
+};
